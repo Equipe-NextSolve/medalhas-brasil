21b38f25a580780103a2051c15160df371334dd2
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-03-18 21:11:52 -0300

    Adição do Favcon do Site

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,16 @@
 import "./globals.css";
 import Header from "@/layout/Header/Header";
 import Footer from "@/layout/Footer/Footer";
+import { Icon } from "lucide-react";
 
 export const metadata = {
   title: "Medalhas Brasil - Site Oficial",
   description: "Site de Medalhas, Trofeús, e acessórios comemorativos para Eventos, campeonatos e premiações.",
+  icon: {
+    Icon: '/logo.png'
+  }
 };
+
 
 export default function RootLayout({ children }) {
   return (
