fb086f234ba186b93e4736bce7ff0674edbd926e
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-05-04 15:49:40 -0300

    Melhorando o carrossel do site, trocando as imagens que tinham direito autoral

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -2,71 +2,72 @@
 import React from 'react'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, Autoplay } from 'swiper/modules'
+import { Pagination, Autoplay } from 'swiper/modules'
 
 import 'swiper/css'
-import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 export default function Carousel() {
 
     const imgdesktop = [
-        { id: 1, image: '/slide-descktop-01.png', alt: 'Slide 1' },
-        { id: 2, image: '/slide-descktop-02.png', alt: 'Slide 2' },
-        { id: 3, image: '/slide-descktop-03.png', alt: 'Slide 3' },
+        { id: 1, image: '/banner11.png', alt: 'Slide 1' },
+        { id: 2, image: '/banner1.png', alt: 'Slide 2' },
     ]
 
     const imgmobile = [
-        { id: 1, image: '/slide-mobile-01.png', alt: 'Slide 1' },
-        { id: 2, image: '/slide-mobile-02.png', alt: 'Slide 2' },
-        { id: 3, image: '/slide-mobile-03.png', alt: 'Slide 3' },
+        { id: 1, image: '/mobile1.png', alt: 'Slide 1' },
+        { id: 2, image: '/mobile2.png', alt: 'Slide 2' },
     ]
 
     return (
-        <section className="w-full min-h-[700] pt-20">
 
-            <div className="hidden md:block w-full h-200">
+        <section className="w-full pt-24">
+
+            <div className="hidden md:block w-full" style={{ aspectRatio: '1920/650' }}>
                 <Swiper
                     modules={[Pagination, Autoplay]}
                     slidesPerView={1}
-                    navigation
                     pagination={{ clickable: true }}
-                    autoplay={{ delay: 3000 }}
+                    autoplay={{ delay: 3000, disableOnInteraction: false }}
                     loop
                     className="w-full h-full"
                 >
                     {imgdesktop.map((item) => (
                         <SwiperSlide key={item.id}>
-                            <div className="relative w-full h-200">
+                            <div className="relative w-full h-full">
                                 <Image
                                     src={item.image}
                                     fill
                                     alt={item.alt}
-                                    className="object-container"
+                                    className="object-cover"
                                     priority
+                                    sizes="100vw"
+                                    quality={100}
                                 />
                             </div>
                         </SwiperSlide>
                     ))}
                 </Swiper>
             </div>
-
-            <div className="block md:hidden">
+            <div className="block md:hidden w-full" style={{ aspectRatio: '9/16' }}>
                 <Swiper
-                    modules={[Navigation, Pagination, Autoplay]}
+                    modules={[Pagination, Autoplay]}
                     slidesPerView={1}
                     pagination={{ clickable: true }}
-                    autoplay={{ delay: 4000 }}
+                    autoplay={{ delay: 4000, disableOnInteraction: false }}
                     loop
+                    className="w-full h-full"
                 >
                     {imgmobile.map((item) => (
-                        <SwiperSlide key={item.id}>
+                        <SwiperSlide key={item.id} className="relative w-full h-full">
                             <Image
                                 src={item.image}
-                                width={600}
-                                height={850}
+                                fill
                                 alt={item.alt}
-                                className="w-full h-auto object-cover"
+                                className="object-cover"
+                                priority
+                                sizes="100vw"
+                                quality={100}
                             />
                         </SwiperSlide>
                     ))}
